4f01e10af157f54508f0d90966b86cf5567e9715
Tzvi Spitz <tes5884@users.noreply.github.com>
2021-10-20 12:42:45 -0400

    Update RemoveFCC.ps1
    fixed quotation marks

--- RemoveFCC.ps1
+++ RemoveFCC.ps1
@@ -1,7 +1,7 @@
 $exists = Test-Path -Path $env:LOCALAPPDATA\FCC\fcc_uninstaller.exe -PathType Leaf
 if ($exists -eq $true){
     Write-Output "FCC is installed, I will now remove it"
-    $env:LOCALAPPDATA\FCC\fcc_uninstaller.exe /S /currentuser
+    "$env:LOCALAPPDATA\FCC\fcc_uninstaller.exe /S /currentuser"
     Remove-Item $env:LOCALAPPDATA\FCC -Force -Recurse
     }
     else {
